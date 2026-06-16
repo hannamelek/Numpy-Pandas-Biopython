09a3c778084febab6f9dc72336049fc94f3702ba
Hanna Melek Henry <thewaysofthegeek@gmail.com>
2026-06-16 11:43:49 +0530

    entrez 13

--- entrez13.py
+++ entrez13.py
@@ -9,13 +9,13 @@ from Bio import Entrez
 
 # In[2]:
 
-
+# Set your email address (required by NCBI)
 Entrez.email = "hannamelek826@gmail.com"
 
 
 # In[5]:
 
-
+# Fetch the E. coli genome sequence in FASTA format
 handle = Entrez.efetch(
     db="nucleotide",
     id="NC_000913.3",
@@ -30,10 +30,11 @@ print(sequence[:500])
 
 # In[6]:
 
-
+#"bacterial_gene.fasta" = file name
+#"w" = write mode
 with open("ecoli.fasta", "w") as file:
     file.write(sequence)
-
+#puts the contents of the variable sequence into the file.
 
 # In[8]:
 
@@ -52,6 +53,8 @@ print(len(record.seq))
 
 # In[11]:
 
+# Fetch the E. coli genome sequence in FASTA format directly into a SeqRecord object, (fetch and parse directly)
+#You don't need to save the file. You can fetch the sequence and parse it directly into a SeqRecord object using SeqIO.read() with the handle from Entrez.efetch().
 
 from Bio import Entrez, SeqIO
 
@@ -72,7 +75,7 @@ print(len(record.seq))
 
 # In[13]:
 
-
+#Suppose you don't know the accession number.
 from Bio import Entrez
 
 Entrez.email = "hannamelek826@gmail.com"
